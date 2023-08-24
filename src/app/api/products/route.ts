cad9f5be9f9fd03295a5de08b701e54913ae1212
jssatorres <juansataz.dev@gmail.com>
2023-08-24 17:40:18 +0200

    feat: POST product WIP

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -15,6 +15,75 @@ export async function GET() {
   }
 }
 
-export function POST() {
-  return NextResponse.json('creando')
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    // const data = await request.formData();
+    // const image = data.get("image");
+
+    // if (!data.get("name")) {
+    //   return NextResponse.json(
+    //     {
+    //       message: "Name is required",
+    //     },
+    //     {
+    //       status: 400,
+    //     }
+    //   );
+    // }
+
+    // if (!image) {
+    //   return NextResponse.json(
+    //     {
+    //       message: "Image is required",
+    //     },
+    //     {
+    //       status: 400,
+    //     }
+    //   );
+    // }
+
+    // const buffer = await processImage(image);
+
+    // const res = await new Promise((resolve, reject) => {
+    //   cloudinary.uploader
+    //     .upload_stream(
+    //       {
+    //         resource_type: "image",
+    //       },
+    //       async (err, result) => {
+    //         if (err) {
+    //           console.log(err);
+    //           reject(err);
+    //         }
+
+    //         resolve(result);
+    //       }
+    //     )
+    //     .end(buffer);
+    // });
+
+    // const result = await conn.query("INSERT INTO product SET ?", {
+    //   name: data.get("name"),
+    //   description: data.get("description"),
+    //   price: data.get("price"),
+    //   image: res.secure_url,
+    // });
+
+    // return NextResponse.json({
+    //   name: data.get("name"),
+    //   description: data.get("description"),
+    //   price: data.get("price"),
+    //   id: result.insertId,
+    // });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
